0e2052d9f8291aad9dc1f29b5ba50f9e05ee99d2
Tasnim Zotder <tasnimzotder@gmail.com>
2020-09-26 02:35:02 +0530

    add: Python - find greater

---
+++ python/find-greater.py
@@ -0,0 +1,10 @@
+# Finf the greater number
+
+def find_greater(x, y):
+    # Todo: Write a function to return greater number
+
+
+x = int(input())
+y = int(input())
+
+print('Greater Number: {}'.format(find_greater(x, y)))
