65f4a7c142f227b26a2c323b0875b41ceb350f7f
harjot9812 <harjot9812@gmail.com>
2020-09-25 13:39:54 +0530

    Update even_odd.py
    completed function

--- python/even_odd.py
+++ python/even_odd.py
@@ -1,7 +1,11 @@
 # Function to check if a number is even or odd
 
-def even_odd(x):
+def even_odd(n):
     # Todo: Code to determine & print even or odd
+    if(n%2==0):
+        print("even")
+    else:
+        print ("odd")
 
 
 even_odd(5)
