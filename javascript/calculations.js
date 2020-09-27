fdf137d0217307c5a66f770c49db8a8835af9f65
Tasnim Zotder <tasnimzotder@gmail.com>
2020-09-27 10:37:52 +0530

    JS - Calculations

---
+++ javascript/calculations.js
@@ -0,0 +1,25 @@
+// Calculations -> Add, Subtract, Multiply, Divide
+
+function add(x, y) {
+    // Todo: Write a function to return the addition of x and y
+}
+
+function subtract(x, y) {
+    // Todo: Write a function to return the subtraction of x and y
+}
+
+function multiply(x, y) {
+    // Todo: Write a function to return the mulltiplication of x and y
+}
+
+function divide(x, y) {
+    // Todo: Write a function to return the division of x and y
+}
+
+var x = 13;
+var y = 4;
+
+console.log(`Add: ${add(x, y)}`);
+console.log(`Subtract: ${subtract(x, y)}`);
+console.log(`Multiply: ${multiply(x, y)}`);
+console.log(`Divide: ${divide(x, y)}`);
