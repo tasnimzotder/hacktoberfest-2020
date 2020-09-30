79c1a81870b6ab074a9171e6cd60764fc6626d83
Sandeep N <60995458+sandeepN06@users.noreply.github.com>
2020-10-01 00:48:32 +0530

    palindrome check of numbers included

--- python/palindrome_checker.py
+++ python/palindrome_checker.py
@@ -3,6 +3,8 @@
                             Palindrome checker by python
 
 '''
+#Palindrome check for String inputs.
+
 def palindrome (s):
     r=s[::-1]
     if(r==s):
@@ -11,4 +13,18 @@ def palindrome (s):
         print("No")
 s = input()
 palindrome(s)
+
+#Palindrome check for Number (Integer) inputs.
+
+num=int(input("Enter a number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
 
