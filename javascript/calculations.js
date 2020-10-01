e78c372d33c5d6f05bfafd8f743e62dee166b16b
Gauravsaha-97 <gauravsaha385@gmail.com>
2020-10-01 10:38:28 +0530

    Completed the functions Add, Subtract, Multiply and Divide

--- javascript/calculations.js
+++ javascript/calculations.js
@@ -2,18 +2,22 @@
 
 function add(x, y) {
     // Todo: Write a function to return the addition of x and y
+    return x + y;
 }
 
 function subtract(x, y) {
     // Todo: Write a function to return the subtraction of x and y
+    return x - y;
 }
 
 function multiply(x, y) {
     // Todo: Write a function to return the mulltiplication of x and y
+    return x * y;
 }
 
 function divide(x, y) {
     // Todo: Write a function to return the division of x and y
+    return x / y;
 }
 
 var x = 13;
