2e5c6a72182ca96932f6c894249eebe94e6976ff
Pranav N <pranavbharadwaj142@gmail.com>
2020-10-01 11:34:32 +0530

    Update calculations.js

--- javascript/calculations.js
+++ javascript/calculations.js
@@ -1,19 +1,19 @@
 // Calculations -> Add, Subtract, Multiply, Divide
 
 function add(x, y) {
-    // Todo: Write a function to return the addition of x and y
+    return x+y
 }
 
 function subtract(x, y) {
-    // Todo: Write a function to return the subtraction of x and y
+    return x-y
 }
 
 function multiply(x, y) {
-    // Todo: Write a function to return the mulltiplication of x and y
+    return x*y
 }
 
 function divide(x, y) {
-    // Todo: Write a function to return the division of x and y
+    return x/y
 }
 
 var x = 13;
