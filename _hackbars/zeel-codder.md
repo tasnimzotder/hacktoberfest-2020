cb7658371669e2d886268571a87f0a50abf3e9d9
zeel-codder <zeelprajapati321@gmial.com>
2020-10-03 10:08:10 +0530

    My first Commit on git

---
+++ _hackbars/zeel-codder.md
@@ -0,0 +1,14 @@
+---
+layout: hackbar
+title: My First PR
+author: zeel-codder
+---
+
+<!-- Your MESSAGE on open source-->
+I always too wanted to contribute in open source. And help others.
+
+---
+
+## We all have to wake up for save over mother Earth for good future.<!-- Your MESSAGE on climate change -->
+
+![Wake_Up]({{site.baseurl}}/assets/images/zeel_codder.jpg)
--- python/Spilt.py
+++ python/Spilt.py
@@ -1,17 +1,26 @@
 #split function is used for creat string in to list of String's
 
-String="10 11 12 13 14 15 16"
+# String="10 11 12 13 14 15 16"
 
 #we have to give Raguler Expression as argument of split function
 #Know split function will split over string with respact" "
 #l=["10","11","12","13","14","15","16"]
 
-l=String.split(" ")
+# l=String.split(" ")
 
-String="   10 12 13   "
+# String="   10 12 13   "
 
 #strip function is used for remove Extra space in given string
 #befor function String ="   10 12 13   "
 #after function String="10 12 13"
 
-String.strip()
+# String.strip()
+
+# code..
+
+s=input("Enter String to Split!!!:")
+c=input("Enter Char which Respact to Split!!:")
+print("List:",s.split(c))
+s=input("Enter String :")
+print("After Strip String:"+s.strip())
+
