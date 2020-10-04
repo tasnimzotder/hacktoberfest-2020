281f73c2339cc4e2c35eb2bb582054c788d7406a
Tasnim Zotder <hello@tasnim.dev>
2020-10-04 19:04:04 +0530

    Update strongnumber.py

--- python/strongnumber.py
+++ python/strongnumber.py
@@ -1,14 +1,16 @@
 def strongnumber(n):
     _sum=0
     r=0
+    
     while n!=0:
         r=n%10
         fact=1
         
         while r!=0:
             fact=fact*r
-            r=r-1          
-        _sum=_sum+fact    
+            r=r-1
+            
+        _sum=_sum+fact 
         n=n//10
     return sum
 
