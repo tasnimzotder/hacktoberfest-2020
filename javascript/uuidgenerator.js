c6f01e78ffb28af1a9e281a70dbb23de8d449eb4
Tasnim Zotder <tasnimzotder@gmail.com>
2020-10-05 10:03:20 +0530

    Update uuidgenerator.js

--- javascript/uuidgenerator.js
+++ javascript/uuidgenerator.js
@@ -1,4 +1,5 @@
-/* eslint no-console: "error" */
+/*eslint quotes: ["error", "single"]*/
+/*eslint-env es6*/
 
 // npm install uuid
 
@@ -8,8 +9,8 @@
 // using version 4 to create uuid
 // generates a random uuid
 
-const { v4: uuidv4 } = require("uuid");
-Console.log(`${uuidv4()}`);
+const { v4: uuidv4, v5: uuidv5 } = require('uuid');
+console.log(uuidv4());
 
 // output
 // da8f5839-63f0-4c25-a7b1-a0172a03904b
@@ -22,8 +23,9 @@ Console.log(`${uuidv4()}`);
 // using version 5 to create uuid
 // generates a random uuid
 
-const { v5: uuidv5 } = require("uuid");
-Console.log(`${uuidv5()}`);
+const MY_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+console.log(uuidv5('Hello, World!', MY_NAMESPACE));
 
 // output
 // 53508957-8e61-44d6-97e2-db4e5b8947f3
+
