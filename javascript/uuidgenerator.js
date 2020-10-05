2aa9c220cc25dad70b6638b097f331da4767e217
Tasnim Zotder <tasnimzotder@gmail.com>
2020-10-05 09:32:13 +0530

    Update uuidgenerator.js

--- javascript/uuidgenerator.js
+++ javascript/uuidgenerator.js
@@ -5,11 +5,10 @@
 /* ----------------------------------- */
 
 // using version 4 to create uuid
-
 // generates a random uuid
 
-`` const { v4: uuidv4 } = require('uuid'); ``
-/* console.log(`${uuidv4()}`) */
+const { v4: uuidv4 } = require('uuid');
+console.log(`${uuidv4()}`);
 
 // output
 // da8f5839-63f0-4c25-a7b1-a0172a03904b
@@ -19,13 +18,11 @@
 
 // here we use only v4,
 // to use v5 uncomment it using ctrl + /
-
 // using version 5 to create uuid
-
 // generates a random uuid
 
-// const { v5: uuidv5 } = require('uuid');
-// console.log(`${uuidv5()}`)
+const { v5: uuidv5 } = require('uuid');
+console.log(`${uuidv5()}`);
 
 // output
 // 53508957-8e61-44d6-97e2-db4e5b8947f3
